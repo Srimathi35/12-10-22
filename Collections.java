e15d5560e3f07f1b380f6929f37798d2e220f91b
Srimathi35 <110155702+Srimathi35@users.noreply.github.com>
2022-10-17 16:15:13 +0530

    Add files via upload

---
+++ Collections.java
@@ -0,0 +1,19 @@
+package com.collection.demo;
+
+public class Collections {
+
+	public static void main(String[] args) {
+		
+				String s1 ="Hi, Welcome to this Mentoring Session";
+
+				for(String word: s1.split(" ")) {
+
+					System.out.println(word);
+				}
+			}
+
+		
+
+	}
+
+
